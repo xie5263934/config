e59197ceef527f31bb1f3e96aec4ea729f84baa0
sheldonxie <jinrun.xie@56qq.com>
2018-07-11 18:13:09 +0800

    提交

--- src/main/java/com/test/spring/cloud/App.java
+++ src/main/java/com/test/spring/cloud/App.java
@@ -2,6 +2,7 @@ package com.test.spring.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class App {
     public static void main(String[] args) {
 
